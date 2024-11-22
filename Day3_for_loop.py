66998a65d0ad3ddc8c5b51b541cd1f89169edc0a
Jobk2006 <jobmon7@gmail.com>
2024-11-22 21:57:22 +0530

    Day3_for_loop.py

--- Day3_for_loop.py
+++ Day3_for_loop.py
@@ -1,6 +1,2 @@
-# write a python program that shows the multiplication table of 2 (1-10)
+# write a python program to display the multiplication table of 2 (1-10)
 # using for loop 
-mul2=0
-for i in range(1,11)
-mul2=i*2
-print (f"2*{i}={mul2})
