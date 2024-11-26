01e9dcf767eeedc69a68647feb828ed18b0139f7
meenakshisijo <meenakshisijo7@gmail.com>
2024-11-26 21:02:49 +0530

    Update Day3_for_loop.py

--- Day3_for_loop.py
+++ Day3_for_loop.py
@@ -1,8 +1,7 @@
 # write a python program to display the multiplication table of 2 (1-10)
 # using for loop.
-# author Meenakshi Sijo 
-num=int(input("enter the value of num:"))
-num=2
+# author Meenakshi Sijo
+
 for i in range(1,11):
    print(f"{num*i}")
       
