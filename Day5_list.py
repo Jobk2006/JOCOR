19896444d9ae38d985c22118c426d636ad46aa5e
Jobk2006 <jobmon7@gmail.com>
2024-11-27 21:09:21 +0530

    Day5_list.py

---
+++ Day5_list.py
@@ -0,0 +1,3 @@
+# 1 create a list days with elements 'Monday', 'Tuesday', 'Wednesday', 'Friday', 'Saturday'
+# 2 Add the element Sunday at last of the list using append operator
+# 3 Insert Thursday at the right place using slice operator 
