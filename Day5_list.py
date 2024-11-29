49fbea24f8bcb9b1bb0d74d6bb9959ad3340219e
TSHRISANT <shrisants82@gmail.com>
2024-11-29 07:45:29 +0530

    Update Day5_list.py

--- Day5_list.py
+++ Day5_list.py
@@ -1,3 +1,5 @@
-# 1 create a list days with elements 'Monday', 'Tuesday', 'Wednesday', 'Friday', 'Saturday'
-# 2 Add the element Sunday at last of the list using append operator
-# 3 Insert Thursday at the right place using slice operator 
+
+days = ['Monday', 'Tuesday', 'Wednesday', 'Friday', 'Saturday']
+days.append('Sunday')
+days[3:3] = ['Thursday']
+print("Updated list of days:", days)
