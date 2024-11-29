9e6304067d2bf57d9de4dd7a04e03e470cfcf2fb
meenakshisijo <meenakshisijo7@gmail.com>
2024-11-29 21:18:54 +0530

    Update Day4_while.py

--- Day4_while.py
+++ Day4_while.py
@@ -1,1 +1,10 @@
 #find the sum of first 10 even number using while loop 
+# AuthorMeenakshi Sijo
+sum=0
+count=1
+  even_numbers=2
+while count<=10:
+  sum+=even_numbers
+  even_numbers+=2
+  count+=1
+  print("sum")
