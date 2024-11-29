08f07868ab5546ce11662091dbe3016b245a9f64
meenakshisijo <meenakshisijo7@gmail.com>
2024-11-29 21:42:17 +0530

    Update Day5_list.py

--- Day5_list.py
+++ Day5_list.py
@@ -3,3 +3,9 @@ days = ['Monday', 'Tuesday', 'Wednesday', 'Friday', 'Saturday']
 days.append('Sunday')
 days[3:3] = ['Thursday']
 print("Updated list of days:", days)
+#author Meenakshi Sijo
+days=['Monday','tuesday','wednesday','friday','saturday']
+ days.append('sunday')
+  print (sunday)
+ days.insert(3,'thursday'(
+  print(days)
