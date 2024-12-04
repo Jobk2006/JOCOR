a176e21345a346ee3c761e7a64a99484c553df4e
TSHRISANT <shrisants82@gmail.com>
2024-12-04 20:31:54 +0530

    Update Day5_list.py

--- Day5_list.py
+++ Day5_list.py
@@ -1,6 +1,13 @@
 
 days = ['Monday', 'Tuesday', 'Wednesday', 'Friday', 'Saturday']
 days.append('Sunday')
+days = days[:3] + ['Thursday'] + days[3:]
+#author shrisant 
+print(days)
+
+
+days = ['Monday', 'Tuesday', 'Wednesday', 'Friday', 'Saturday']
+days.append('Sunday')
 days[3:3] = ['Thursday']
 print("Updated list of days:", days)
 #author Meenakshi Sijo
