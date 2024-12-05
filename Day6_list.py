bf5f2ef2da387b9d59782be34f04eaeb874d8ab5
Jobk2006 <jobmon7@gmail.com>
2024-12-05 11:56:48 +0530

    Update Day6_list.py

--- Day6_list.py
+++ Day6_list.py
@@ -1,3 +1,3 @@
 #write a python program to create two lists [1,3,5,7] and [2,4,6,8] 
-# add the list using concatenating 
+# add the two lists using concatenating 
 # print thr 5th element using slicing operation
