207de1df4cd057bfb391aa95de8a480b722dbf3b
Suhailmather <suhailmather781@gmail.com>
2024-12-05 21:38:26 +0530

    Day5_list.py

--- Day5_list.py
+++ Day5_list.py
@@ -15,3 +15,22 @@ days=['Monday','tuesday','wednesday','friday','saturday']
   print (sunday)
  days[3:3]=['thursday']
 print(days)
+
+
+#author suhail
+days = ['Monday', 'Tuesday', 'Wednesday', 'Friday', 'Saturday']
+days.append('Sunday')
+days = days[:3] + ['Thursday'] + days[3:] 
+print(days)
+
+
+days = ['Monday', 'Tuesday', 'Wednesday', 'Friday', 'Saturday']
+days.append('Sunday')
+days[3:3] = ['Thursday']
+print("Updated list of days:", days)
+#author Meenakshi Sijo
+days=['Monday','tuesday','wednesday','friday','saturday']
+ days.append('sunday')
+  print (sunday)
+ days[3:3]=['thursday']
+print(days)
