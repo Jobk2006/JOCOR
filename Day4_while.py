3a6273399f895926971a9723dd8ccbbddabade2b
Suhailmather <suhailmather781@gmail.com>
2024-12-05 21:37:10 +0530

    Day4_while.py

--- Day4_while.py
+++ Day4_while.py
@@ -7,4 +7,15 @@ while count<=10:
   sum+=even_numbers
   even_numbers+=2
   count+=1
+print("sum")
+
+
+# Authorsuhail
+sum=0
+count=1
+  even_numbers=2
+while count<=10:
+  sum+=even_numbers
+  even_numbers+=2
+  count+=1
   print("sum")
