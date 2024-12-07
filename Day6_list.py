74013296dbf6e6d5a1f4e0c177d6453c11d1f23d
Suhailmather <suhailmather781@gmail.com>
2024-12-07 11:47:56 +0530

    Day6_list.py

--- Day6_list.py
+++ Day6_list.py
@@ -9,3 +9,15 @@ print("5th Element:", fifth_element)
 #write a python program to create two lists [1,3,5,7] and [2,4,6,8] 
 # add the two lists using concatenating 
 # print thr 5th element using slicing operation
+
+
+list1 = [1, 3, 5, 7]
+list2 = [2, 4, 6,8]
+combined_list = list1 + list2
+fifth_element = combined_list[4]  
+print("Concatenated List:", combined_list)
+print("5th Element:", fifth_element)
+#author suhail
+#write a python program to create two lists [1,3,5,7] and [2,4,6,8] 
+# add the two lists using concatenating 
+# print thr 5th element using slicing operation
