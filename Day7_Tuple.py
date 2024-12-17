7dbee60af79b4d7af43a0794ec22cf6510aa1cb1
Jobk2006 <jobmon7@gmail.com>
2024-12-17 23:34:57 +0530

    Day7_Tuple.py

---
+++ Day7_Tuple.py
@@ -0,0 +1,7 @@
+#Tuple
+#1 Create a tuple containing the names of 5 cities.
+
+#2 Allow the user to:
+
+#Display all cities in the tuple.
+#Search for a city by name and check if it exists in the tuple.
