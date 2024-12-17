5e6ae4417a80f0bac1eb7b45ea074f93a041f050
Jobk2006 <jobmon7@gmail.com>
2024-12-17 23:40:19 +0530

    Day8_tuple.py

---
+++ Day8_tuple.py
@@ -0,0 +1,8 @@
+# 1.Define a tuple named animals with the values ("cat", "dog", "elephant", "tiger", "lion").
+
+# 2.Perform the following tasks:
+
+#Print the 3rd element of the animals tuple.
+#Print the 2nd to last element of the animals tuple using negative indexing.
+#Print a slice of the tuple to get the middle three elements.
+
