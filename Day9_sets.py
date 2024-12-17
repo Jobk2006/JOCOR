c1f4597ec327030a94cf0a915ce531e0a8028d46
Jobk2006 <jobmon7@gmail.com>
2024-12-17 23:42:56 +0530

    Day9_sets.py

---
+++ Day9_sets.py
@@ -0,0 +1,9 @@
+#Create two sets:
+#set_a = {1, 2, 3, 4, 5}
+#set_b = {4, 5, 6, 7, 8}
+#Perform the following operations:
+#Find the union of the two sets and print it.
+#Find the intersection of the two sets and print it.
+#Find the difference between set_a and set_b and print it.
+#Find the symmetric difference between the two sets and print it.
+
